0660b4ebd341dc5e76531d0feb6d71965d3fcc10
Phay Wang <xbl@live.co.uk>
2022-09-22 02:40:24 +0100

    Update app.py

--- driving_accompany/app.py
+++ driving_accompany/app.py
@@ -1,9 +1,9 @@
-from tkinter import Button
+
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime 
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
-from flask import Flask,Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash
+from flask_login import login_user, current_user
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 today = datetime.now()
 app = Flask(__name__)
